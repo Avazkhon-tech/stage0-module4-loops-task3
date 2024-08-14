308d6aab1a557e2cecb2543e75b7ee7419e5e29e
Avazkhon <avazxonnazirov334@gmail.com>
2024-08-14 12:23:49 +0500

    second

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,12 +3,15 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
 
+
         int length = Integer.toString(t).length() - 1;
+        t = t < 0 ? t * -1: t;
         int sum = 0;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i <= length; i++) {
             sum += t % 10;
             t = t / 10;
         }
+
         System.out.println(sum);
     }
 }
